5cc5af52bdace68ab1a027dd6eb0f99ea4c8118a
Grant Dickie <gdickie@npr.org>
2017-05-18 13:01:57 -0400

    Adding in help text for when a story is not found

--- index.js
+++ index.js
@@ -1,70 +1,112 @@
 var request = require('request');
 
-var checkIfThingPublished = (event, callback) => {
-    try {
-      if (event.text === 'undefined') {
-        callback("No thingId specified");
-        return;
-      }
-      var ApiKey = process.env.storyapi;
-      var url = "https://api.npr.org/query?apiKey=" + ApiKey + "&id=" + event.thingid + "&output=JSON";
-      request({
-        url: url,
-        method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          json: true
-      }, function(err, res, body) {
-        if (err) {
-          callback(err);
-          return;
-        }
-        if (body !== null) {
-          // Body should be JSON
-            if (body.list.story && body.list.story.length) {
-                var date = body.list.story[0].pubDate.$text,
-                link = null;
-                // Getting story link here
-                for (var i=0; i < body.list.story[0].link.length; i++) {
-                    if (body.list.story[0].link[i].type == "html") {
-                        link = body.list.story[0].link[i].$text;
-                        break;
-                    }
-                 }
+const HELP_TEXT = "Hi there! Ask me anything about whether something published or not."
 
-                callback(null, {
-                    date: date,
-                    link: link
-                });
-            } else {
-                callback("No story found");
-            }
+var checkIfThingPublished = (event, callback) =>
+{
+    try {
+        if (event.text === 'undefined') {
+            callback("No thingId specified");
+            return;
         }
-      });
+        var ApiKey = process.env.storyapi;
+        var url = "https://api.npr.org/query?apiKey=" + ApiKey + "&id=" + event.text + "&output=JSON";
+        console.log("Url:", url)
+        request({
+            url: url,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            json: true
+        }, function (err, res, body) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            if (body !== null) {
+                // Body should be JSON
+                if (body.list.story && body.list.story.length) {
+                    var date = body.list.story[0].pubDate.$text,
+                        link = null;
+                    // Getting story link here
+                    for (var i = 0; i < body.list.story[0].link.length; i++) {
+                        if (body.list.story[0].link[i].type == "html") {
+                            link = body.list.story[0].link[i].$text;
+                            break;
+                        }
+                    }
+
+                    callback(null, {
+                        date: date,
+                        link: link
+                    });
+                } else {
+                    noStoryFoundMessage(event, callback);
+                }
+            } else {
+                noStoryFoundMessage(event, callback);
+            }
+        });
     } catch (err) {
-      callback(err.message);
+        callback(err.message);
     }
 }
 
+var noStoryFoundMessage = (json, callback) => {
+    request({
+        url: process.env.webhookUrl,
+        method: 'POST',
+        json: true,
+        body: {
+            "text": "No Story Found!",
+            "attachments": [{
+                "fallback": "Could not find a published story in our API system matching the ID " + json.text,
+                "fields": [{
+                    "Reason": "No story was found in the story API matching " + json.text
+                }]
+            }]
+        }
+    }, function (err, res, body) {
+        if (err) {
+            callback(null, {"text": err.message});
+        }
+        callback(null);
+    });
+}
 
-var getItemsFromMessyBody = (body, callback) => {
+var sendBackHelpText = (callback) => {
+    try {
+        callback(null, { "text": HELP_TEXT });
+    } catch (err) {
+        console.log(err);
+        callback(null, { "text": err.message });
+    }
+}
+
+var getItemsFromMessyBody = (body, callback) =
+>
+{
     try {
         var token = body.match(/token=([A-z0-9]+)&/)[1],
             command = body.match(/command=%2F([A-z]+)&/)[1],
-            text = body.match(/text=([0-9]+)&/)[1];
+            text = body.match(/text=([0-9]+)&/)[1],
+            responseUrl = body.match(/response_url=([A-z0-9.%]+)/)[1].replace(/%2F/g, '/').replace(/%3A/g, ':');
         callback(null, {
             token: token,
             command: command,
-            text: text
+            text: text,
+            responseUrl: responseUrl
         });
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         callback(err);
     }
 }
 
-var formatForSlack = (results, callback) => {
+var formatForSlack = (results, responseUrl, callback) =
+>
+{
     try {
         var formattedText = {
             "text": "Last published on: " + results.date
@@ -75,30 +117,41 @@ var formatForSlack = (results, callback) => {
                 "title_link": results.link
             }];
         }
-        callback(null, formattedText);
+        console.log("response url: ", process.env.webhookUrl);
+        request({
+            url: process.env.webhookUrl,
+            method: 'POST',
+            body: formattedText,
+            json: true
+        }, callback);
     } catch (err) {
         console.log(err);
-        callback("An error occurred");
+        callback(null, "An error occurred " + err.message);
     }
-
 }
 
-exports.handler = (event, context, callback) => {
+exports.handler = (event, context, callback) =>
+{
+    console.log("Event: ", event);
     try {
-        getItemsFromMessyBody(event.body, function(err, json) {
+        getItemsFromMessyBody(event.body, function (err, json) {
             if (err) {
-                throw new err(err.message);
+                callback(null, {
+                    "err": err.message
+                })
             }
-            if (json.command == "diditpublish") {
-                checkIfThingPublished(json, function(err, results) {
+            console.log("json", json);
+            if (json.command == "diditpublish" && json.text) {
+                checkIfThingPublished(json, function (err, results) {
                     if (err !== null) {
                         callback(err);
                     }
+                    console.log("results: ", results);
 
-                    formatForSlack(results, callback);
+                    formatForSlack(results, json.responseUrl, callback);
                 })
             } else {
-                callback(null, 'Hello from Lambda');
+                sendBackHelpText(callback);
             }
         });
     } catch (err) {
@@ -107,4 +160,5 @@ exports.handler = (event, context, callback) => {
             "text": "An error occurred: " + err.message
         });
     }
-};
+}
+;
